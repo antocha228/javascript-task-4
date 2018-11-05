b0e0ffa7eb1a7cb87c6a604433ffe4c11496b751
Anton Vlasov <anton.vlasov@jetstyle.ru>
2018-11-05 23:05:39 +0500

    свинка, не подведи

--- emitter.js
+++ emitter.js
@@ -4,13 +4,32 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-const isStar = true;
+const isStar = false;
+
+function handle(context) {
+    for (let [student, handlers] of context) {
+        handlers.forEach(handler => handler.call(student));
+    }
+}
+
+function getEvents(event) {
+    let events = event.split('.');
+
+    if (events[1]) {
+        events[1] = `${events[0]}.${events[1]}`;
+        events.reverse();
+    }
+
+    return events;
+}
 
 /**
  * Возвращает новый emitter
  * @returns {Object}
  */
 function getEmitter() {
+    let subscriptions = new Map();
+
     return {
 
         /**
@@ -18,26 +37,50 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object}
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            if (!subscriptions[event]) {
+                subscriptions[event] = new Map();
+            }
+            if (!subscriptions[event].get(context)) {
+                subscriptions[event].set(context, []);
+            }
+            subscriptions[event].get(context).push(handler);
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object}
          */
         off: function (event, context) {
-            console.info(event, context);
+            for (const ev in subscriptions) {
+                if (ev === event || ev.startsWith(`${event}.`)) {
+                    subscriptions[ev].delete(context);
+                }
+            }
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object}
          */
         emit: function (event) {
-            console.info(event);
+            const events = getEvents(event);
+
+            events.forEach(ev => subscriptions[ev]
+                ? handle(subscriptions[ev])
+                : undefined
+            );
+
+            return this;
         },
 
         /**
